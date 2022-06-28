2762284d2496e275977849fb79de14b0fc35d3bc
Talha-Jabbar-SDE <talha.jabbar@logicielservice.com>
2022-06-28 17:01:31 +0500

    console app converted to default empty web api template

---
+++ ConsoleToWebAPI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace ConsoleToWebAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webHost =>
+                    {
+                        webHost.UseStartup<Startup>();
+                    });
+        }
+    }
+}
---
+++ ConsoleToWebAPI/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleToWebAPI
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(); // now this application has the support of controllers (injecting Web API service).
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+            app.UseRouting(); // enabling routing in this application
+
+            app.UseEndpoints(endpoints => 
+            {
+                /*
+                endpoints.MapGet("/", async context => 
+                {
+                    await context.Response.WriteAsync("Hello from new Web API App");
+                });
+                */
+                // After adding services.AddControllers() in ConfigureServices method we are dealing with controllers so output should be generated from controllers classes.
+                // So:
+                endpoints.MapControllers();
+
+                endpoints.MapGet("/test", async context =>
+                {
+                    await context.Response.WriteAsync("Hello from new Web API App test");
+                });
+            });
+        }
+    }
+}

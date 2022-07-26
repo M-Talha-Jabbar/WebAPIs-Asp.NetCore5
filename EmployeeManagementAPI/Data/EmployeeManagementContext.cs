b0a94ed6a0063f4d1cb6579804d8880b8d4a41a0
Talha-Jabbar-SDE <talha.jabbar@logicielservice.com>
2022-07-26 16:13:18 +0500

    ef core db-first approach updated

---
+++ EmployeeManagementAPI/Data/EmployeeManagementContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using EmployeeManagementAPI.Models;
+
+#nullable disable
+
+namespace EmployeeManagementAPI.Data
+{
+    public partial class EmployeeManagementContext : DbContext
+    {
+        public EmployeeManagementContext()
+        {
+        }
+
+        public EmployeeManagementContext(DbContextOptions<EmployeeManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Departments { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.;Database=EmployeeDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasOne(d => d.Department)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.DepartmentId)
+                    .HasConstraintName("FK_Employees_Departments");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ EmployeeManagementAPI/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EmployeeManagementAPI.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? DepartmentId { get; set; }
+
+        public virtual Department Department { get; set; }
+    }
+}
